7733071417c01b8e44abcdbffbec904a08b2e16b
navyd <dhjnavyd@gmail.com>
2019-03-02 22:18:11 +0800

    Sort Array By Parity II 通过

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 242  | [Valid Anagram](https://leetcode.com/problems/valid-anagram/)                                 | Easy | 2    | sort
 349  | [Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays)        | Easy | 2    | sort
 350  | [Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/) | Easy | 2    | sort
+922  | [Sort Array By Parity II](https://leetcode.com/problems/sort-array-by-parity-ii/)             | Easy | 1    | sort
---
+++ src/main/java/cn/navyd/leetcode/sort/SortArrayByParityII.java
@@ -0,0 +1,60 @@
+package cn.navyd.leetcode.sort;
+
+/**
+ * <pre>
+Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+
+Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+
+You may return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+ 
+
+Note:
+
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+ * </pre>
+ * @author navyd
+ *
+ */
+public class SortArrayByParityII {
+  /**
+   * 思路：遍历数组，当元素为奇数时，保存新数组的奇数位置，反之亦然。
+   * @param A
+   * @return
+   */
+  public int[] sortArrayByParityII(int[] A) {
+    final int len = A.length;
+    // 奇数起始下标， 偶数起始下标
+    int o = 1, e = 0;
+    int[] r = new int[len];
+    for (int i = 0; i < len; i++) {
+      int val = A[i];
+      if (isOdd(val)) {
+        r[o] = val;
+        o += 2;
+      } else {
+        r[e] = val;
+        e += 2;
+      }
+    }
+    return r;
+  }
+
+  static boolean isOdd(int n) {
+    return (n & 0x1) == 1;
+  }
+
+  public static void main(String[] args) {
+    System.err.println(isOdd(1));
+  }
+}
