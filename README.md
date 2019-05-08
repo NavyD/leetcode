c1b43cf2dea4b927657e1e204a3271b66496e308
navyd <dhjnavyd@gmail.com>
2019-05-08 21:24:36 +0800

    添加问题KthLargestElementinanArray及4种解法

--- README.md
+++ README.md
@@ -4,18 +4,19 @@
 
 尽量保证每天更新一个Accepted，但是不可能保证每天一个题。
 
-序号 | 题名                                                                                          | 难度   | 解法 | 标签
------|-----------------------------------------------------------------------------------------------|--------|------|-----
-242  | [Valid Anagram](https://leetcode.com/problems/valid-anagram/)                                 | Easy   | 2    | sort
-349  | [Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays)        | Easy   | 2    | sort
-350  | [Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/) | Easy   | 2    | sort
-922  | [Sort Array By Parity II](https://leetcode.com/problems/sort-array-by-parity-ii/)             | Easy   | 1    | sort
-973  | [K Closest Points to Origin](https://leetcode.com/problems/k-closest-points-to-origin/)       | Easy   | 2    | sort
-976  | [Largest Perimeter Triangle](https://leetcode.com/problems/largest-perimeter-triangle/)       | Easy   | 1    | sort
-56   | [Merge Intervals](https://leetcode.com/problems/merge-intervals/)                             | Medium | 1    | sort
-75   | [Sort Colors](https://leetcode.com/problems/sort-colors/)                                     | Medium | 2    | sort
-147  | [Insertion Sort List](https://leetcode.com/problems/insertion-sort-list/)                     | Medium | 2    | sort
-148  | [Sort List](https://leetcode.com/problems/sort-list/)                                         | Medium | 2    | sort
-179  | [Largest Number](https://leetcode.com/problems/largest-number/)                               | Medium | 1    | sort
-274  | [H-Index](https://leetcode.com/problems/h-index/)                                             | Medium | 1    | sort
-274  | [HWiggleSortII](https://leetcode.com/problems/wiggle-sort-ii/)                                             | Medium | 2    | sort
+序号 | 题名                                                                                              | 难度   | 解法 | 标签
+-----|---------------------------------------------------------------------------------------------------|--------|------|-----
+242  | [Valid Anagram](https://leetcode.com/problems/valid-anagram/)                                     | Easy   | 2    | sort
+349  | [Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays)            | Easy   | 2    | sort
+350  | [Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/)     | Easy   | 2    | sort
+922  | [Sort Array By Parity II](https://leetcode.com/problems/sort-array-by-parity-ii/)                 | Easy   | 1    | sort
+973  | [K Closest Points to Origin](https://leetcode.com/problems/k-closest-points-to-origin/)           | Easy   | 2    | sort
+976  | [Largest Perimeter Triangle](https://leetcode.com/problems/largest-perimeter-triangle/)           | Easy   | 1    | sort
+56   | [Merge Intervals](https://leetcode.com/problems/merge-intervals/)                                 | Medium | 1    | sort
+75   | [Sort Colors](https://leetcode.com/problems/sort-colors/)                                         | Medium | 2    | sort
+147  | [Insertion Sort List](https://leetcode.com/problems/insertion-sort-list/)                         | Medium | 2    | sort
+148  | [Sort List](https://leetcode.com/problems/sort-list/)                                             | Medium | 2    | sort
+179  | [Largest Number](https://leetcode.com/problems/largest-number/)                                   | Medium | 1    | sort
+274  | [H-Index](https://leetcode.com/problems/h-index/)                                                 | Medium | 1    | sort
+324  | [Wiggle Sort II](https://leetcode.com/problems/wiggle-sort-ii/)                                   | Medium | 2    | sort
+215  | [Kth Largest Element in an Array](https://leetcode.com/problems/kth-largest-element-in-an-array/) | Medium | 4    | heap
---
+++ src/main/java/cn/navyd/leetcode/heap/KthLargestElementinanArray.java
@@ -0,0 +1,177 @@
+package cn.navyd.leetcode.heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import cn.navyd.annotation.leetcode.Optimal;
+import cn.navyd.annotation.leetcode.Problem;
+import cn.navyd.annotation.leetcode.Problem.Difficulty;
+import cn.navyd.annotation.leetcode.Problem.Tag;
+import cn.navyd.annotation.leetcode.Solution;
+import cn.navyd.annotation.leetcode.Solution.Complexity;
+import cn.navyd.annotation.leetcode.Solution.Status;
+
+/**
+ * <pre>
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+ * </pre>
+ * 
+ * @author navyd
+ *
+ */
+@Problem(number = 215, difficulty = Difficulty.MEDIUM, tags = Tag.HEAP, 
+url = "https://leetcode.com/problems/kth-largest-element-in-an-array/")
+public interface KthLargestElementinanArray {
+  /**
+   * 从未排序的数组中找出第k大（不去重）的元素
+   * 
+   * @param nums
+   * @param k
+   * @return
+   */
+  public int findKthLargest(int[] nums, int k);
+
+  @Solution(status = Status.ACCEPTED, tags = Tag.SORT, 
+      spaceComplexity = Complexity.O_1, timeComplexity = Complexity.O_N_LOG_N, 
+      runtime = 3, runtimeBeatRate = 84.91, memory = 36.2, memoryBeatRate = 95.99, 
+      dates = "2019-05-08",
+      referenceUrls = "https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained")
+  public static class SolutionBySort implements KthLargestElementinanArray {
+    /**
+     * 排序后直接访问数组最后第k个元素即可。适用于小数组
+     */
+    @Override
+    public int findKthLargest(int[] nums, int k) {
+      Arrays.sort(nums);
+      int index = nums.length - k;
+      return nums[index];
+    }
+  }
+  
+  @Solution(status = Status.ACCEPTED, 
+      spaceComplexity = Complexity.O_N, timeComplexity = Complexity.O_N_LOG_N, 
+      runtime = 6, runtimeBeatRate = 63.22, memory = 36.9, memoryBeatRate = 94.64,
+      dates = "2019-05-08",
+      referenceUrls = "https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained")
+  public static class SolutionByPriorityQueue implements KthLargestElementinanArray {
+
+    /**
+     * 使用优先队列保存前k个元素。在添加num到队列时，如果当前queue.size>k，则移除最大的一个元素以保证O(K)个队列元素
+     * <p>时间复杂度：O(K log N). 删除最大元素O(log N)，可能有k次删除操作
+     * <p>注意：当k>nums.length/2时，使用max priorityqueue能够减少poll操作，相反 k<nums.length/2，min priority queue同样减少poll
+     */
+    @Override
+    public int findKthLargest(int[] nums, int k) {
+      // 避免频繁扩容操作
+      Queue<Integer> queue = new PriorityQueue<>(k+1);
+      for (int num : nums) {
+        queue.offer(num);
+        // 保证k个元素
+        if (queue.size() > k)
+          queue.poll();
+      }
+      return queue.peek();
+    }
+  }
+  
+  @Solution(status = Status.ACCEPTED, tags = Tag.SORT, 
+    spaceComplexity = Complexity.O_1, timeComplexity = Complexity.O_N, 
+    runtime = 5, runtimeBeatRate = 67.82, memory = 36.1, memoryBeatRate = 96.13,
+    dates = "2019-05-08",
+    referenceUrls = "https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained")
+  public static class SolutionByPartition implements KthLargestElementinanArray {
+
+    /**
+     * 通过 快速选择 将数组nums的前k个元素小，后k个元素大的结构，第k个元素则是第k大
+     * <p>如果不使用shuffle 时间复杂度将会出现最坏情况，runtime=23 ms
+     */
+    @Override
+    public int findKthLargest(int[] nums, int k) {
+      shuffle(nums);
+      int lo = 0, hi = nums.length-1;
+      // 将第k大 转化为 数组下标k
+      k = nums.length - k;
+      while (lo < hi) {
+        int index = partition(nums, lo, hi);
+        if (index < k)
+          lo = index + 1;
+        else if (index > k)
+          hi = index - 1;
+        else 
+          break;
+      }
+      return nums[k];
+    }
+    
+    static final Random RAND = new Random();
+    
+    void shuffle(int[] nums) {
+      for (int i = 0; i < nums.length; i++)
+        swap(nums, i, RAND.nextInt(nums.length));
+    }
+     
+    int partition(int[] nums, int lo, int hi) {
+      final int pivot = nums[lo];
+      int i = lo, j = hi+1;
+      while (true) {
+        while (nums[++i] <= pivot)
+          if (i == hi)
+            break;
+        while (nums[--j] > pivot)
+          ;
+        if (i >= j)
+          break;
+        swap(nums, i, j);
+      }
+      swap(nums, lo, j);
+      return j;
+    }
+    
+    static void swap(int[] nums, int i, int j) {
+      int tmp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = tmp;
+    }
+  }
+  
+  @Optimal
+  @Solution(status = Status.ACCEPTED, tags = Tag.SORT, 
+      spaceComplexity = Complexity.O_1, timeComplexity = Complexity.O_N, 
+      runtime = 3, runtimeBeatRate = 84.91, memory = 36.4, memoryBeatRate = 95.45,
+      dates = "2019-05-08",
+      referenceUrls = {
+          "https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained/267106",
+          "https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60312/AC-Clean-QuickSelect-Java-solution-avg.-O(n)-time/61582"})
+  public static class SolutionByEasierPartition extends SolutionByPartition {
+    /**
+     * 一个简洁易读的partition。
+     * 在一个for中将<pivot的交换到前段lo，>pivot则暂时不动，等到遇到新的<pivot则交换，导致>pivot的移动到右边，
+     * <p>注意，这样有可能导致以移动>pivot的元素再次被后边的<pivot元素交换，导致额外的开销，对于大数组不可取
+     */
+    @Override
+    int partition(int[] nums, int lo, int hi) {
+      final int pivot = nums[hi];
+      int i = lo;
+      for (int j = lo; j < hi; j++) {
+        if (nums[j] <= pivot)
+          swap(nums, i++, j);
+      }
+      swap(nums, i, hi);
+      return i;
+    }
+  }
+}
+
+
