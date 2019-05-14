a44018066e7b89a98b2f637e9449fb146b8df154
navyd <dhjnavyd@gmail.com>
2019-05-14 13:36:55 +0800

    修改HIndex。第2次提交答案HIndex.SolutionByBucketSort

--- src/main/java/cn/navyd/leetcode/sort/HIndex.java
+++ src/main/java/cn/navyd/leetcode/sort/HIndex.java
@@ -1,6 +1,14 @@
 package cn.navyd.leetcode.sort;
 
-import java.util.Arrays;
+import cn.navyd.annotation.leetcode.Author;
+import cn.navyd.annotation.leetcode.Problem;
+import cn.navyd.annotation.leetcode.Problem.Difficulty;
+import cn.navyd.annotation.leetcode.Problem.Tag;
+import cn.navyd.annotation.leetcode.Solution;
+import cn.navyd.annotation.leetcode.Solution.Complexity;
+import cn.navyd.annotation.leetcode.Submission;
+import cn.navyd.annotation.leetcode.Submission.Status;
+import cn.navyd.annotation.leetcode.Unskilled;
 
 /**
 <pre>
@@ -21,63 +29,60 @@ Note: If there are several possible values for h, the maximum one is taken as th
  * @author navyd
  *
  */
+@Unskilled
+@Problem(title = "H-Index", number = 274, difficulty = Difficulty.MEDIUM, tags = Tag.SORT, 
+url = "https://leetcode.com/problems/h-index/")
 public interface HIndex {
-  int hIndex(int[] citations);
-}
-
-@SuppressWarnings("unused")
-class MySolutionHIndex implements HIndex {
-
-  @Override
-  public int hIndex(int[] citations) {
-    
-    // 排序
-    Arrays.sort(citations);
-    // 设置h 与 min min条件一直成立min >= h
-    final int len = citations.length;
-    for (int i = 0; i < len; i++) {
-      int hIndex = citations[i], max = len - hIndex, remainingPaper;
-      
-      for (int j = len - 1; j >= i; j--) {
-        int citation = citations[j];
-      }
-    }
-    // h=a[0] min = h, max = N - h;
-    
-    // 比较max <= arr.max则成立 否则减小max
-    return 0;
-  }
-}
-
-class SolutionByBucketSort implements HIndex {
-
   /**
-   * 思路：由于h-index被定义的最大为论文数量n。使用桶排序buckets时定义length=n+1，下标为[0,n]，论文citation
-   * 可以对应buckets[citation]，重复则++，对于citation>=n的则仅buckets[n]++，不区分>n，因为h-index不可能>n
-   * 然后从最后迭代buckets并计数count，当遇到第一个count>=i即找到h-index=i
-   * 
-   * 时间复杂度：O(N)
-   * 空间复杂度：O(N)
+   * h-index被定义为第一个 被引用的次数h >= 被引用论文的数量之和。
+   * 也就是说 {@code h<=citations.length}
+   * @param citations
+   * @return
    */
-  @Override
-  public int hIndex(int[] citations) {
-    // 入桶
-    final int n = citations.length;
-    // 最大桶为数组length，h-index不会超过length，citation>length的仅++即可
-    int[] buckets = new int[n+1];
-    for (int citation : citations)
-      if (citation >= n)
-        buckets[n]++;
-      else 
-        buckets[citation]++;
-    // 遍历桶 寻找当bucket index <= count桶计数时，h-index被找到
-    int count = 0;
-    for (int i = n; i >= 0; i--) {
-      count += buckets[i];
-      if (count >= i)
-        return i;
-    }
-    return 0;
-  }
+  public int hIndex(int[] citations); 
   
+  
+  @Author(name = "yfcheng", significant = true, 
+      referenceUrls = "https://leetcode.com/problems/h-index/discuss/70768/Java-bucket-sort-O(n)-solution-with-detail-explanation")
+  @Submission(date = "2019-05-14", status = Status.ACCEPTED,
+      runtime = 0, runtimeBeatRate = 100.00, memory = 34.9, memoryBeatRate = 93.48,
+      url = "https://leetcode.com/submissions/detail/228785686/")
+  // submission中无法查询到memoryBeatRate，超过能够显示的内存
+  @Submission(date = "2019-04-12",
+      runtime = 0, runtimeBeatRate = 100.00, memory = 37.7, memoryBeatRate = 0.00,
+      url = "https://leetcode.com/submissions/detail/221903890/")
+  @Solution(tags = Tag.SORT_BUCKET, timeComplexity = Complexity.O_N, spaceComplexity = Complexity.O_N)
+  public static class SolutionByBucketSort implements HIndex {
+    /**
+     * 思路：统计每个citation对应的论文数量，然后从大的citation遍历，对应的数量之和超过citation时，这个citation就是h-index，
+     * 保证了每个论文至少有引用citation个。
+     * <p>实现：
+     * <p>使用桶排序，每个桶保存同citation的论文数量。
+     * <p>将citation分为n+1个桶，从0桶开始。citation >= n的保存到n桶中，由于h不可能超过论文数量，统计超过的数量即可
+     * <p>从前往后遍历，统计citation对应的论文数量之和count，如果count>=citation即得到h-index: citation
+     */
+    @Override
+    public int hIndex(int[] citations) {
+      final int n = citations.length;
+      // 分配bucket
+      int[] buckets = new int[n+1];
+      // 统计每个citation桶的对应论文数量
+      for (int citation : citations) {
+        // 超过的引用数量 在一起统计
+        if (citation >= n)
+          buckets[n]++;
+        else 
+          buckets[citation]++;
+      }
+      int paperCount = 0;
+      for (int citation = n; citation >= 0; citation--) {
+        // 统计论文数量 查找论文数量第一个超过citation的citation
+        paperCount += buckets[citation];
+        if (paperCount >= citation)
+          return citation;
+      }
+      return 0;
+    }
+    
+  }
 }
