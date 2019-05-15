c857243982229ddfe1f66d16349369bce0bc8965
navyd <dhjnavyd@gmail.com>
2019-05-15 10:31:24 +0800

    增加答案HIndex.SolutionBySort及变体SolutionBySortEndToStart

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 147  | [Insertion Sort List](https://leetcode.com/problems/insertion-sort-list/)                         | Medium | 2    | sort
 148  | [Sort List](https://leetcode.com/problems/sort-list/)                                             | Medium | 2    | sort
 179  | [Largest Number](https://leetcode.com/problems/largest-number/)                                   | Medium | 1    | sort
-274  | [H-Index](https://leetcode.com/problems/h-index/)                                                 | Medium | 1    | sort
+274  | [H-Index](https://leetcode.com/problems/h-index/)                                                 | Medium | 3    | sort
 324  | [Wiggle Sort II](https://leetcode.com/problems/wiggle-sort-ii/)                                   | Medium | 4    | sort
 220  | [Contains Duplicate III](https://leetcode.com/problems/contains-duplicate-iii/)                   | Medium | 3    | sort
 215  | [Kth Largest Element in an Array](https://leetcode.com/problems/kth-largest-element-in-an-array/) | Medium | 4    | heap
--- src/main/java/cn/navyd/leetcode/sort/HIndex.java
+++ src/main/java/cn/navyd/leetcode/sort/HIndex.java
@@ -1,5 +1,6 @@
 package cn.navyd.leetcode.sort;
 
+import java.util.Arrays;
 import cn.navyd.annotation.leetcode.Author;
 import cn.navyd.annotation.leetcode.Problem;
 import cn.navyd.annotation.leetcode.Problem.Difficulty;
@@ -27,7 +28,7 @@ Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of 
 Note: If there are several possible values for h, the maximum one is taken as the h-index.
 </pre>
  * @author navyd
- *
+ * ps: 这个题目有点难懂,h-index的定义有点混乱
  */
 @Unskilled
 @Problem(title = "H-Index", number = 274, difficulty = Difficulty.MEDIUM, tags = Tag.SORT, 
@@ -57,7 +58,7 @@ public interface HIndex {
      * 思路：统计每个citation对应的论文数量，然后从大的citation遍历，对应的数量之和超过citation时，这个citation就是h-index，
      * 保证了每个论文至少有引用citation个。
      * <p>实现：
-     * <p>使用桶排序，每个桶保存同citation的论文数量。
+     * <p>使用桶排序，每个桶保存同citation的论文数量。此时的citation不只是桶的下标，也是论文的数量
      * <p>将citation分为n+1个桶，从0桶开始。citation >= n的保存到n桶中，由于h不可能超过论文数量，统计超过的数量即可
      * <p>从前往后遍历，统计citation对应的论文数量之和count，如果count>=citation即得到h-index: citation
      */
@@ -76,12 +77,72 @@ public interface HIndex {
       }
       int paperCount = 0;
       for (int citation = n; citation >= 0; citation--) {
-        // 统计论文数量 查找论文数量第一个超过citation的citation
+        // 统计论文数量
         paperCount += buckets[citation];
+        // 如果总的论文数量超过当前的citation，则返回数量citation个
         if (paperCount >= citation)
           return citation;
       }
       return 0;
+    }
+  }
+  
+  @Author(name = "han35", referenceUrls = "https://leetcode.com/problems/h-index/discuss/70808/Simple-Java-solution-with-sort/73010")
+  @Author(name = "novice00", significant = true, referenceUrls = "https://leetcode.com/problems/h-index/discuss/70808/Simple-Java-solution-with-sort")
+  @Submission(date = "2019-05-15", status = Status.ACCEPTED, 
+  runtime = 1, runtimeBeatRate = 81.61, memory = 34.5, memoryBeatRate = 99.67,
+  url = "https://leetcode.com/submissions/detail/228978719/")
+  @Solution(timeComplexity = Complexity.O_N_LOG_N, spaceComplexity = Complexity.O_1)
+  public static class SolutionBySort implements HIndex {
+
+    /**
+     * 思路：将citations排序，从前往后遍历，统计论文数量。
+     * 假设所有论文都符合，即citations[0]>=n。
+     * <p>如果遇到citation < count，说明该存在一个论文不符合，count--不计算（移除）最小的论文citation
+     * <p>如果发现存在一个citation >= count，表示至少count个论文的引用都>=citation，之前的n-count <= citation
+     */
+    @Override
+    public int hIndex(int[] citations) {
+      Arrays.sort(citations);
+      final int n = citations.length;
+      // 假设所有论文 都符合
+      int paperCount = n;
+      for (int i = 0; i < n; i++) {
+        // 如果第一个citation >= count表示以后的citation都>count 该count是最大的
+        if (paperCount <= citations[i])
+          break;
+        // 该count不符合citation
+        paperCount--;
+      }
+      return paperCount;
+    }
+  }
+  
+  @Author(name = "oreomilkshake", referenceUrls = "https://leetcode.com/problems/h-index/discuss/70808/Simple-Java-solution-with-sort/73008")
+  @Submission(date = "2019-05-15", status = Status.ACCEPTED, 
+  runtime = 1, runtimeBeatRate = 81.61, memory = 35.1, memoryBeatRate = 98.91,
+  url = "https://leetcode.com/submissions/detail/228970601/")
+  @Solution(timeComplexity = Complexity.O_N_LOG_N, spaceComplexity = Complexity.O_1, derived = SolutionBySort.class)
+  public static class SolutionBySortEndToStart implements HIndex {
+
+    /**
+     * 假设所有论文都不符合，当从后往前遍历时，遇到count < citation，则说明符合h-index
+     * 当遇到第一个count >= citation说明count是最大的
+     */
+    @Override
+    public int hIndex(int[] citations) {
+      Arrays.sort(citations);
+      final int n = citations.length;
+      // 假设所有论文 不都符合
+      int paperCount = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        // 引用数量大于论文数量
+        if (paperCount < citations[i]) 
+          paperCount++;
+        else 
+          break;
+      }
+      return paperCount;
     }
     
   }
