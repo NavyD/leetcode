24ce8f66c5f2605a7a7e63ed0aa3b722f3a596d3
navyd <dhjnavyd@gmail.com>
2019-05-28 13:13:52 +0800

    添加解答SolutionByCountingSort, SolutionByPriorityQueueII

--- src/main/java/cn/navyd/leetcode/sort/ReorganizeString.java
+++ src/main/java/cn/navyd/leetcode/sort/ReorganizeString.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import cn.navyd.annotation.leetcode.Author;
+import cn.navyd.annotation.leetcode.DerivedFrom;
+import cn.navyd.annotation.leetcode.Optimal;
 import cn.navyd.annotation.leetcode.Problem;
 import cn.navyd.annotation.leetcode.Problem.Difficulty;
 import cn.navyd.annotation.leetcode.Problem.Tag;
 import cn.navyd.annotation.leetcode.Solution;
 import cn.navyd.annotation.leetcode.Solution.Complexity;
 import cn.navyd.annotation.leetcode.Submission;
-import cn.navyd.annotation.leetcode.Submission.Status;
 
 /**
  * <pre>
@@ -40,6 +41,7 @@ S will consist of lowercase letters and have length in range [1, 500].
 public interface ReorganizeString {
   /**
    * 将字符串S重新排列，使相邻字符不是在重复的字符
+   * <p>复杂度：假设S的长度为N，平均每个字符的数量为K
    * @param S
    * @return
    */
@@ -51,7 +53,7 @@ public interface ReorganizeString {
       referenceUrls = "https://leetcode.com/problems/reorganize-string/discuss/113440/Java-solution-PriorityQueue")
   @Submission(date = "2019-05-25", runtime = 36, runtimeBeatRate = 33.31, memory = 35.2,
       memoryBeatRate = 92.22, url = "https://leetcode.com/submissions/detail/231188436/")
-  @Solution(timeComplexity = Complexity.O_K_N_LOG_N, spaceComplexity = Complexity.O_N_K)
+  @Solution(timeComplexity = Complexity.O_N_LOG_K, spaceComplexity = Complexity.O_N)
   public static class SolutionByPriorityQueue implements ReorganizeString {
 
     /**
@@ -65,10 +67,9 @@ public interface ReorganizeString {
      * <li>否则需要重新获取下一个字符作为当前插入字符比较
      * <li>如果不存在下一个则说明不能重排
      * </ul>
-     * 假设S的字符种类为N，平均长度为K，（s.length相当于NK）一次优先队列操作为O(log N)。
      * <p>时间复杂度：
-     * 重排需要循环S.length次即NK次，每次队列操作为log N。即O(KN log N)
-     * <p>空间复杂度：将S转为char[]需要O(NK)，queue和map都需要O(N)
+     * 重排需要循环次N次，每次队列操作为log K。即O(N log K)
+     * <p>空间复杂度：将S转为char[], stringbuilder需要O(N)，queue和map常数26
      */
     @Override
     public String reorganizeString(String S) {
@@ -108,12 +109,122 @@ public interface ReorganizeString {
       }
       return reorganized.toString();
     }
+  }
+  
+  @DerivedFrom(SolutionByPriorityQueue.class)
+  @Author(name = "FionaFang", significant = true,
+      referenceUrls = "https://leetcode.com/problems/reorganize-string/discuss/128907/Java-solution-99-similar-to-358")
+  @Submission(date = "2019-05-28", runtime = 35, runtimeBeatRate = 38.92, memory = 35.2,
+      memoryBeatRate = 92.10, url = "https://leetcode.com/submissions/detail/231887633/")
+  @Solution(timeComplexity = Complexity.O_N_LOG_K, spaceComplexity = Complexity.O_N)
+  public static class SolutionByPriorityQueueII implements ReorganizeString {
+
+    /**
+     * 与{@link SolutionByPriorityQueue}主要不同在于在重排时，
+     * 避免在最后一个字符相同时，避免再次poll,offer操作。
+     * <p>重排时优先队列两次poll必然是不同的字符，不再需要判断是否与之前的一致
+     * <p>使用引用prev保存之前poll的值，交替发送到stringbuiler
+     * <p>交替-1不会导致相同的字符被重复poll发送到结果中
+     * <p>如果不是可重排的，则queue 重复的字符prev不会被offer，
+     * 提前为空，导致prev的剩余字符数量不会被发送到结果中
+     * <p>时间复杂度：priority queue的offer poll操作为 O(log K)，重排需要重复N次，则为O(N log K)
+     * <p>
+     * 空间复杂度：queue, map都是固定空间26，S toCharArray与结果stringbuilder需要O(N)的空间
+     */
+    @Override
+    public String reorganizeString(String S) {
+      final int countLimit = 26;
+      // 统计
+      final Map<Character, Integer> charCounts = new HashMap<>(countLimit);
+      for (char c : S.toCharArray())
+        charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+      // 构建priory queue
+      final Queue<Map.Entry<Character, Integer>> queue =
+          new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+      queue.addAll(charCounts.entrySet());
+      // 重排
+      final StringBuilder reorganized = new StringBuilder();
+      // 交替poll
+      Map.Entry<Character, Integer> prev = null;
+      while (!queue.isEmpty()) {
+        Map.Entry<Character, Integer> cur = queue.poll();
+        // 将上一个字符重入队列
+        if (prev != null && prev.getValue() > 0)
+          queue.offer(prev);
+        reorganized.append(cur.getKey());
+        cur.setValue(cur.getValue() - 1);
+        prev = cur;
+      }
+      // 如果不是可重排的 字符数量与输入不一致
+      return (reorganized.length() == S.length()) ? reorganized.toString() : "";
+    }
+  }
+  
+  @Optimal
+  @Author(name = "fangbiyi", significant = true,
+      referenceUrls = "https://leetcode.com/problems/reorganize-string/discuss/232469/Java-O(N)-3ms-beat-100")
+  @Submission(date = "2019-05-28", runtime = 1, runtimeBeatRate = 96.72, memory = 33.8,
+      memoryBeatRate = 100.00, url = "https://leetcode.com/submissions/detail/231903207/")
+  @Solution(timeComplexity = Complexity.O_N, spaceComplexity = Complexity.O_N)
+  public static class SolutionByCountingSort implements ReorganizeString {
+
+    /**
+     * 思路：使用计数排序统计每个字符出现的次数，重排时不依赖Priority queue，优先将重复次数最多的字符
+     * 间隔的插入结果字符串中。然后将其余字符也间隔插入剩下的位置中，如果超过了数组长度，则从头开始插入那些间隔
+     * <p>即重排时，间隔的插入大的字符，保证相邻的不会重复，然后将其他字符也间隔插入即可
+     * <p>时间复杂度：遍历counts常数时间，S字符遍历O(N)，重排时counts常数时间，每次遍历K次，故最大为S遍历O(N)
+     * <p>空间复杂度：S的输出结果数组O(N)
+     */
+    @Override
+    public String reorganizeString(String S) {
+      // 统计计数
+      final int n = S.length();
+      final int[] counts = new int[26];
+      for (char c : S.toCharArray())
+        counts[index(c)]++;
+      // 找到重复次数最多的字符
+      char mostLetter = letter(0);
+      final int letterCountLimit = (S.length() + 1) / 2;
+      for (char i = 0; i < counts.length; i++)
+        if (counts[index(mostLetter)] < counts[i])
+          mostLetter = letter(i);
+      // 检查数量是否合法
+      if (counts[index(mostLetter)] > letterCountLimit)
+        return "";
+      // 重排 最多的
+      final char[] reorganized = new char[n];
+      // 从0开始
+      int intervalIndex = 0;
+      while (counts[index(mostLetter)]-- > 0) {
+        reorganized[intervalIndex] = mostLetter;
+        intervalIndex += 2;
+      }
+      // 重排 剩余的
+      for (int i = 0; i < counts.length; i++) {
+        while (counts[i]-- > 0) {
+          // 如果超过　则从１开始 不能使用%n，因为10%10=0，从0开始了
+          if (intervalIndex >= n)
+            intervalIndex = 1;
+          reorganized[intervalIndex] = letter(i);
+          intervalIndex += 2;
+        }
+      }
+      return String.valueOf(reorganized);
+    }
     
+    static char letter(int index) {
+      return (char)(index + 'a');
+    }
+    
+    static int index(char c) {
+      return c - 'a';
+    }
   }
   
   
   @Author(name = "navyd")
-  @Submission(date = "2019-05-25", status = Status.WRONG, url = "https://leetcode.com/submissions/detail/231179919/")
+  // 编译异常 等待修复
+//  @Submission(date = "2019-05-25", status = Status.WRONG, url = "https://leetcode.com/submissions/detail/231179919/")
   @Solution(timeComplexity = Complexity.O_N_LOG_N, spaceComplexity = Complexity.O_N)
   public static class SolutionBySort implements ReorganizeString {
 
