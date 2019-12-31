ec38b6d065ee1c52f7b341b5719955d0008f3f3d
navyd <dhjnavyd@gmail.com>
2019-12-31 08:52:54 +0000

    rewrite and add solutions: SolutionBySort,SolutionByIterativeMergeToEnd,SolutionByIterativeMergeToEndOriginal

--- src/main/java/cn/navyd/leetcode/sort/MergeIntervals.java
+++ src/main/java/cn/navyd/leetcode/sort/MergeIntervals.java
@@ -3,16 +3,15 @@ package cn.navyd.leetcode.sort;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import cn.navyd.annotation.algorithm.ComplexityEnum;
+import cn.navyd.annotation.algorithm.SortAlgorithm;
+import cn.navyd.annotation.algorithm.TimeComplexity;
 import cn.navyd.annotation.leetcode.Author;
-import cn.navyd.annotation.leetcode.Optimal;
+import cn.navyd.annotation.leetcode.DateTime;
+import cn.navyd.annotation.leetcode.DifficultyEnum;
 import cn.navyd.annotation.leetcode.Problem;
-import cn.navyd.annotation.leetcode.Problem.Difficulty;
-import cn.navyd.annotation.leetcode.Problem.Tag;
-import cn.navyd.annotation.leetcode.Solution;
-import cn.navyd.annotation.leetcode.Solution.Complexity;
 import cn.navyd.annotation.leetcode.Submission;
-import cn.navyd.annotation.leetcode.Submission.Status;
-import cn.navyd.annotation.leetcode.Unskilled;
 
 /**
  * <pre>
@@ -33,8 +32,7 @@ NOTE: input types have been changed on April 15, 2019. Please reset to default c
  * @author navyd
  *
  */
-@Unskilled
-@Problem(number = 56, difficulty = Difficulty.MEDIUM, tags = Tag.SORT, url = "https://leetcode.com/problems/merge-intervals/")
+@Problem(number = 56, difficulty = DifficultyEnum.MEDIUM)
 public interface MergeIntervals {
   /**
    * 合并重叠的区间
@@ -43,16 +41,16 @@ public interface MergeIntervals {
    */
   public int[][] merge(int[][] intervals);
   
-  @Author(name = "brubru777", significant = true, 
-      referenceUrls = "https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution")
-  @Submission(date = "2019-05-16", status = Status.ACCEPTED,
-      runtime = 36, runtimeBeatRate = 41.45, memory = 36, memoryBeatRate = 99.99,
-      url = "https://leetcode.com/submissions/detail/229207368/")
-  @Solution(timeComplexity = Complexity.O_N_LOG_N, spaceComplexity = Complexity.O_N)
+  @Author("navyd")
+  @Submission(memory = 38.1, memoryBeatRate = 98.55, runtime = 6, runtimeBeatRate = 89.20, submittedDate = @DateTime("20191230"), url = "https://leetcode.com/submissions/detail/289604195/")
+  @Author(value = "brubru777", references = "https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution")
+  @Submission(submittedDate = @DateTime("20190516"), runtime = 36, runtimeBeatRate = 41.45, memory = 36, memoryBeatRate = 99.99, url = "https://leetcode.com/submissions/detail/229207368/")
+  @SortAlgorithm(timeComplexity = @TimeComplexity(average = ComplexityEnum.O_N_LOG_N), spaceComplexity = ComplexityEnum.O_N)
   public static class SolutionBySort implements MergeIntervals {
 
     /**
-     * 思路：合并两个重复的区间，如果intervals是根据interval.start有序的，合并区间时，只需要判断当前区间cur.end与下一个区间的next.start的关系即可
+     * 思路：合并两个重复的区间，如果intervals是根据interval.start有序的，合并区间时，只需要判断当前区间cur.end与下一个区间的next.start的关系即可。
+     * 利用sort start的有序性，避免区间合并前后对不相交的情况
      * <ol>
      * <li>如果cur.end >= next.start说明两区间相交。否则不相交
      * <li>如果相交，则cur.end更新为cur next end的较大值
@@ -68,54 +66,67 @@ public interface MergeIntervals {
      */
     @Override
     public int[][] merge(int[][] intervals) {
-      if (intervals == null || intervals.length < 2)
+      if (intervals.length < 2)
         return intervals;
-      // 排序
-      Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
-      // 合并的新数组 size
-      int mergedIntervalsSize = intervals.length;
-      // 当前合并的区间
+      // 0. sort
+      Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+      int count = intervals.length;
+      // 1. iterative
       int[] curInterval = intervals[0];
       for (int i = 1; i < intervals.length; i++) {
-        int[] nextInterval = intervals[i];
-        final int curEnd = curInterval[1], 
-            nextStart = nextInterval[0], 
-            nextEnd = nextInterval[1]; 
-        // 两个区间 相交
-        if (curEnd >= nextStart) {
-          // 将当前区间end置为两区间end较大值
-          curInterval[1] = Integer.max(curEnd, nextEnd);
-          // 将next置为null，标记为已合并的区间
+        int[] that = intervals[i];
+        // 2. merge if is overlap
+        if (curInterval[1] >= that[0]) {
+          curInterval[1] = Math.max(curInterval[1], that[1]);
           intervals[i] = null;
-          // 合并一个区间 size-1
-          mergedIntervalsSize--;
-        } 
-        // 不相交，重置比较区间
+          count--;
+        }
+        // 3. next is not overlap
         else
-          curInterval = nextInterval;
+          curInterval = that;
       }
-      // 返回比较结果
-      int[][] mergedIntervals = new int[mergedIntervalsSize][];
-      for (int i = 0, j = 0; i < intervals.length; i++) {
-        int[] cur = intervals[i];
-        if (cur != null)
-          mergedIntervals[j++] = cur;
+      // 4. get result
+      int[][] res = new int[count][2];
+      for (int[] interval : intervals)
+        if (interval != null)
+          res[--count] = interval;
+      return res;
+    }
+
+    // 初版
+    public int[][] merge1(int[][] intervals) {
+      // 0. sort
+      Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+      // 1. iterative
+      int count = intervals.length;
+      for (int i = 0; i < intervals.length; i++) {
+        int[] interval = intervals[i];
+        if (interval == null)
+          continue;
+        for (int j = i + 1; j < intervals.length; j++) {
+          int[] that = intervals[j];
+          if (interval[1] >= that[0]) {
+            // merge
+            interval[1] = Math.max(interval[1], that[1]);
+            // delete
+            intervals[j] = null;
+            count--;
+          }
+        }
       }
-      return mergedIntervals;
+      // 2. merge if is overlap
+      int[][] res = new int[count][2];
+      for (int[] interval : intervals)
+        if (interval != null)
+          res[--count] = interval;
+      return res;
     }
   }
   
-  @Optimal
-  @Author(name = "vegito2002", 
-      referenceUrls = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively./115420")
-  @Author(name = "baselRus", 
-      referenceUrls = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively./21227")
-  @Author(name = "D_shaw", significant = true, 
-      referenceUrls = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively.")
-  @Submission(date = "2019-05-17", status = Status.ACCEPTED,
-      runtime = 2, runtimeBeatRate = 99.96, memory = 35.9, memoryBeatRate = 99.99,
-      url = "https://leetcode.com/submissions/detail/229427496/")
-  @Solution(timeComplexity = Complexity.O_N_LOG_N, spaceComplexity = Complexity.O_N)
+  @Author(value = "vegito2002", references = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively./115420")
+  @Author(value = "baselRus", references = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively./21227")
+  @Author(value = "D_shaw", references = "https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively.")
+  @Submission(submittedDate = @DateTime("20190517"), runtime = 2, runtimeBeatRate = 99.96, memory = 35.9, memoryBeatRate = 99.99, url = "https://leetcode.com/submissions/detail/229427496/")
   public static class SolutionBySortStartAndEnd implements MergeIntervals {
 
     /**
@@ -177,4 +188,116 @@ public interface MergeIntervals {
     }
     
   }
+
+  @Author(value = "0", references = "https://leetcode.com/submissions/api/detail/56/java/1/")
+  @Author("navyd")
+  @Author(value = "Eric.zhou", references = "https://leetcode-cn.com/problems/merge-intervals/solution/ji-bai-9969de-ni-men-shi-jian-fu-za-du-zui-pi-o2n-/")
+  @Submission(memory = 40.8, memoryBeatRate = 65.94, runtime = 1, runtimeBeatRate = 100, submittedDate = @DateTime("20191231"), url = "https://leetcode.com/submissions/detail/289943744/")
+  @SortAlgorithm(timeComplexity = @TimeComplexity(best = ComplexityEnum.O_N, worst = ComplexityEnum.O_N_POW_2), spaceComplexity = ComplexityEnum.O_N)
+  public static class SolutionByIterativeMergeToEnd implements MergeIntervals {
+    /**
+     * 思路：遍历合并区间到最后。
+     * <p>问题
+     * <p>对一个区间可以正确合并，但是对于后面未合并interval时，此时不是overlap，合并后则是overlap，
+     * 但是对该interval已经遍历过。cur=[0,0]与[1,6]不相交，但是考虑到[0,0],[0,2]=>[0,2]，这种遍历方式
+     * 导致[0,0]被跳过。
+     * <pre>
+     * intervals: [[1,6],[0,0],null,[0,2],null,null,null,null,null,null,null]
+     * count: 3
+     * i: 1
+     * cur: [0,0]
+     * </pre>
+     * <p>解决方式：合并interval到next位置
+     * <p>优化
+     * <ul>
+     * <li>对合并两个区间到后区间位置后再回到外合并区间，而不是对某个区间合并到底，减少了不必要的null循环判断
+     * <li>内循环从i+1开始，由于每个可合并的已经在最后，i前的已经扫描过，所以不会出现跳过的问题。
+     * </ul>
+     * <p>时间复杂度：当intervals没有可合并interval时，duple iterative时间为O(N^2)。当所有intervals可合并一个interval时，
+     * 只有external loop即O(N)
+     * <p>空间复杂度：最差intervals没有可合并interval时复制result数组为O(N)
+     */
+    @Override
+    public int[][] merge(int[][] intervals) {
+      if (intervals.length < 2)
+        return intervals;
+      int count = intervals.length;
+      // 0. duple iterative
+      for (int i = 0; i < intervals.length; i++) {
+        int[] cur = intervals[i];
+        // break if once merged
+        for (int nextIdx = i + 1; nextIdx < intervals.length; nextIdx++) {
+          int[] next = intervals[nextIdx];
+          if (next == null || isDisjoint(cur, next))
+            continue;
+          // 1. merge if is overlap
+          next[0] = Math.min(cur[0], next[0]);
+          next[1] = Math.max(cur[1], next[1]);
+          // 2. remove merged interval for cur
+          intervals[i] = null;
+          count--;
+          // reduce duplicate next interval iterative
+          break;
+        }
+      }
+      // 3. get result
+      int[][] res = new int[count][];
+      for (int[] interval : intervals)
+        if (interval != null)
+          res[--count] = interval;
+      return res;
+    }
+
+    static boolean isDisjoint(int[] cur, int[] next) {
+      return cur[1] < next[0] || next[1] < cur[0];
+    }
+  }
+
+  @Author("navyd")
+  @Author(value = "Eric.zhou", references = "https://leetcode-cn.com/problems/merge-intervals/solution/ji-bai-9969de-ni-men-shi-jian-fu-za-du-zui-pi-o2n-/")
+  @Submission(memory = 40.6, memoryBeatRate = 66.67, runtime = 2, runtimeBeatRate = 99.14, submittedDate = @DateTime("20191231"), url = "https://leetcode.com/submissions/detail/289931007/")
+  @SortAlgorithm(timeComplexity = @TimeComplexity(best = ComplexityEnum.O_N, worst = ComplexityEnum.O_N_POW_2), spaceComplexity = ComplexityEnum.O_N)
+  public static class SolutionByIterativeMergeToEndOriginal implements MergeIntervals {
+    @Override
+    public int[][] merge(int[][] intervals) {
+      if (intervals.length < 2)
+        return intervals;
+      int count = intervals.length;
+      // 0. duplex iterative
+      for (int i = 0; i < intervals.length; i++) {
+        int[] cur = intervals[i];
+        if (cur == null)
+          continue;
+        int curIdx = i;
+        for (int nextIdx = i + 1; nextIdx < intervals.length; nextIdx++) {
+          int[] next = intervals[nextIdx];
+          if (next == null)
+            continue;
+          // 1. merge if is overlap
+          else if (isOverlap(cur, next)) {
+            next[0] = Math.min(cur[0], next[0]);
+            next[1] = Math.max(cur[1], next[1]);
+            // 2. remove merged interval for cur
+            intervals[curIdx] = null;
+            count--;
+            curIdx = nextIdx;
+            cur = next;
+          }
+        }
+      }
+      // 3. get result
+      int[][] res = new int[count][2];
+      for (int[] interval : intervals)
+        if (interval != null)
+          res[--count] = interval;
+      return res;
+    }
+
+    static boolean isOverlap(int[] cur, int[] next) {
+      // cur include or intersect next
+      return (cur[0] <= next[0] && (cur[1] >= next[1] || cur[1] >= next[0]))
+          // next include or intersect cur
+          || (cur[0] > next[0] && (next[1] >= cur[1] || next[1] >= cur[0]));
+    }
+}
 }
